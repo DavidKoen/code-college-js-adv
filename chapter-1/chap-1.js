cdfc0a79f17ebf8113481a6daa07d9fcc4008191
David Koen <51445963+DavidKoen@users.noreply.github.com>
2023-04-05 12:35:13 +0200

    Add files via upload

---
+++ chapter-1/chap-1.js
@@ -0,0 +1,11 @@
+alert("Saying hi from a different file!");
+
+let a = 10; // This variable's value is 10.
+
+// console.log(a); // This will log the a variable's value.
+
+/* This is a paragraph comment. Hello and Goodbye; Hello and Goodbye;
+Hello and Goodbye; Hello and Goodbye; Hello and Goodbye; Hello and Goodbye;
+Hello and Goodbye. */ 
+
+alert("David");
