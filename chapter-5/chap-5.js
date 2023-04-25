821188a0ec712a714e6f23790df2b16e2856d7b8
David Koen <51445963+DavidKoen@users.noreply.github.com>
2023-04-25 16:12:08 +0200

    Add files via upload

---
+++ chapter-5/chap-5.js
@@ -0,0 +1,12 @@
+console.log("\nMath multiplication table")
+const multiplicationTable = [];
+const value = 12;
+
+for (let i = 0; i < value+1; i++) {
+    let tempArr = [];
+    for (let j = 0; j < value+1; j++) {
+        tempArr.push(i*j);
+    }
+    multiplicationTable.push(tempArr);
+}
+console.table(multiplicationTable);
